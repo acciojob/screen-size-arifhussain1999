e08ebce9f1d255cd156e06071752c67b780e5968
Arif Hussain <arifh9821@gmail.com>
2024-09-27 15:03:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const size =document.getElementById('sizeInfo');
+function setSize(){
+	const width=window.innerWidth;
+	const height=window.innerHeight;
+
+	size.innerHTML='Width: ' + width +' and '+ ' Height: ' + height;
+}
+
+window.addEventListener('resize', setSize);
+setSize();
